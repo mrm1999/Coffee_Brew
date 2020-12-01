e537e65f4306d9f7028d2459912bebd3cb26e36a
mrm1999 <mohitrajmunot1999@gmail.com>
2020-12-01 16:23:18 +0530

    Bugs Fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:coffee_brew/screens/wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 class AuthService {
+  
   final FirebaseAuth _auth = FirebaseAuth.instance;
-
   //sign in Anon 
   Future signInAnon() async {
     try {
@@ -15,5 +16,5 @@ class AuthService {
   }
 
 
-  
+
 }
